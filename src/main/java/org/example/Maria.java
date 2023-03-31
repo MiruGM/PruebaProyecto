5a01ef6f33495768fde439d5eea78b88fb1ee835
DIR\m.garzon.montesinos <mgarmon6@gmail.com>
2023-03-31 12:23:05 +0200

    Cambio RamaMaria - Mi gato

--- src/main/java/org/example/Maria.java
+++ src/main/java/org/example/Maria.java
@@ -3,7 +3,8 @@ package org.example;
 public class Maria {
     public static void main(String[] args) {
 
-        System.out.println("Hola soy María");
+        System.out.println("Hola soy María, tengo un gato cochino que le gusta revolcarse en arena");
+
 
     }
 }
